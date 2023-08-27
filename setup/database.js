68ef008f0b878f4767cf2b36f8b348665cef3c40
Arturs Girons <arturs.girons@gmail.com>
2023-08-27 18:51:27 +0300

    chore: Migration

--- setup/database.js
+++ setup/database.js
@@ -1,9 +1,13 @@
-﻿var mongoose = require('mongoose');
-var config = require('../config');
+﻿var mongoose = require("mongoose");
+var config = require("../config");
 
 mongoose.beyondpad = mongoose.createConnection(config.connectionString);
 
-mongoose.beyondpad.on('error', console.error.bind(console, 'Connection to DB error'));
-mongoose.beyondpad.once('open', console.error.bind(console, 'Connected to DB'));
+mongoose.beyondpad.on("error", function () {
+  console.error("<<<Connection to DB error>>>");
+  process.exit(1);
+});
+
+mongoose.beyondpad.once("open", console.log.bind(console, "Connected to DB"));
 
 module.exports = mongoose;
